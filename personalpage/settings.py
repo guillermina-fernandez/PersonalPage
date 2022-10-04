2f831ed8070d79fd30a165f26d441672d337166d
guillermina-fernandez <guillermina.m.fernandez1987@gmail.com>
2022-10-04 17:57:57 -0300

    secret key fix

--- personalpage/settings.py
+++ personalpage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-asdg+36yo-_wljn*39jkybzul4d$c*3&@73#ai86-4$)@j*_^f'
+SECRET_KEY = 'djangosecretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
